3f77aac29037637225ef607be6d7b16717ac0592
JMBTG <james.mellaerts@free.fr>
2015-04-19 13:40:10 +0200

    commit main 1

--- JavaProjectRILA13/src/application/Main.java
+++ JavaProjectRILA13/src/application/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			System.out.println("Bouya");
+			System.out.println("Bouya");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
