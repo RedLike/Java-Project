05e76eed01b8df67d1bd61f2d56727f3bea39de7
JMBTG <james.mellaerts@free.fr>
2015-05-15 18:29:36 +0200

    class movie/format/room

---
+++ Java Project RILA13/src/Common/Format.java
@@ -0,0 +1,60 @@
+package Common;
+
+public class Format {
+	
+	private String label;
+	private String language;
+	private String description;
+	
+	public Format(String label, String language, String description) {
+		super();
+		this.label = label;
+		this.language = language;
+		this.description = description;
+	}
+	
+	//CRUD
+	public void createFormat(String label, String language, String description) {
+		
+	}
+	
+	public void readFormat(){
+		
+	}
+	
+	public void updateFormat(String label, String language, String description){
+		
+	}
+	
+	public void deleteFormat(){
+		
+	}
+
+	//GETTERS & SETTERS
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
