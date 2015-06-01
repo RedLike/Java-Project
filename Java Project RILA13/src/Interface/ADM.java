2b82214de39bbed439889d71a0d3461b1cd615c4
RedLike <RedLike@outlook.fr>
2015-06-01 20:31:57 +0200

    update

--- Java Project RILA13/src/Interface/ADM.java
+++ Java Project RILA13/src/Interface/ADM.java
@@ -2,10 +2,22 @@ package Interface;
 
 public interface ADM {
 
+	/**
+	 * Return a boolean that is false if the object already exist in the database or if the request doesn't succeed
+	 * @return res
+	 */
 	public boolean create();
 	
+	/**
+	 * Return a boolean that is false if the object don't exist in the database or if the request doesn't succeed
+	 * @return res
+	 */
 	public boolean update();
 	
+	/**
+	  Return a boolean that is false if the object don't exist in the database or if the request doesn't succeed
+	 * @return res
+	 */
 	public boolean delete();
 	
 }
